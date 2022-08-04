270321b61492e22bc96a85ab33058bb562314099
Niklas Bogensperger <niklasbogensperger@users.noreply.github.com>
2022-08-04 20:11:50 +0200

    Add alias for adding a spacer to the dock

--- configs/aliases.zsh
+++ configs/aliases.zsh
@@ -11,6 +11,7 @@ alias ping='prettyping'
 alias raworphans='python3 /Users/niklasbogensperger/Documents/Projects/\[local\]\ \[python\]\ Raw\ Orphans/removeRawOrphans.py'
 
 # for quick access to frequent workflows
+alias dockspacer="defaults write com.apple.dock persistent-apps -array-add '{\"tile-type\"=\"small-spacer-tile\";}' && killall Dock"
 alias scrubexif='exiftool -all='
 alias setdate='exiftool "-alldates<filename"'
 alias setname='exiftool -d "%Y-%m-%d %H.%M.%S.%%e" "-filename<CreateDate"'
