da26dcf67cd852bcd97eaba4e475bbf8273f0edb
Niklas Bogensperger <niklasbogensperger@users.noreply.github.com>
2022-08-12 16:23:00 +0200

    en-/decryptfile: cosmetic output and wording tweaks

--- functions/01_decryptfile.zsh
+++ functions/01_decryptfile.zsh
@@ -1,17 +1,18 @@
 # Decrypting a file using gpg/gnupg
 function decryptfile() {
-    printf 'The encrypted file will be deleted after decryption. To re-encrypt, run encryptfile on the file again.\n'
+    # use ANSI red color code for an extra warning
+    printf 'The encrypted file will be \033[0;31mDELETED\033[0m after decryption. To re-encrypt, run encryptfile on the file again.\n'
     read 'CONTINUE?Do you want to continue [y/N]? '
-    printf '\n'
     if [[ $CONTINUE =~ ^[Yy]$ ]]; then
+        # "%.*" needed to remove (custom) .gpgenc file ending (see 01_encryptfile.zsh); see https://stackoverflow.com/a/34952009
         if gpg --output "${1%.*}" --decrypt --no-symkey-cache "$1"; then
             rm -rf "$1"
         else
-            printf 'Something went wrong during decryption or the process was aborted. The encrypted file was not deleted.\n'
+            printf 'Something went wrong during decryption or the process was aborted. The input file was not deleted.\n'
             return 1
         fi
     else
-        printf 'Your reply was something else than Y or y, so the process was aborted. The encrypted file was not deleted.\n'
+        printf 'Your reply was something else than Y or y, so the process was aborted. The input file was not deleted.\n'
         return 1
     fi
 }
--- functions/01_encryptfile.zsh
+++ functions/01_encryptfile.zsh
@@ -1,17 +1,17 @@
 # Encrypting a file using gpg/gnupg
 function encryptfile() {
-    printf 'The original file will be deleted after encryption and will be unrecoverable without the password! To decrypt, run decryptfile on the file.\n'
+    # use ANSI red color code for an extra warning
+    printf "The original file will be \033[0;31mDELETED\033[0m after encryption and will be \033[0;31mUNRECOVERABLE\033[0m without the password! To decrypt, run decryptfile on the file.\n"
     read 'CONTINUE?Do you want to continue [y/N]? '
-    printf '\n'
     if [[ $CONTINUE =~ ^[Yy]$ ]]; then
         if gpg --output "${1}.gpgenc" --symmetric --cipher-algo AES256 --no-symkey-cache "$1"; then
             rm -rf "$1"
         else
-            printf 'Something went wrong during encryption or the process was aborted. The original file was not deleted.\n'
+            printf 'Something went wrong during encryption or the process was aborted. The input file was not deleted.\n'
             return 1
         fi
     else
-        printf 'Your reply was something else than Y or y, so the process was aborted. The original file was not deleted.\n'
+        printf 'Your reply was something else than Y or y, so the process was aborted. The input file was not deleted.\n'
         return 1
     fi
 }
