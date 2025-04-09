b2953fc27cdb8a1729e6e9ea99797fdca47d8b2c
Niklas Bogensperger <niklasbogensperger@users.noreply.github.com>
2025-04-09 18:53:01 +0200

    Update path.zsh
    Big restructuring:
    - homebrew on apple silicon uses a new activation method
    - python default environment is now a venv managed by uv ("externally managed" python installs have pip disabled)
    - TeXLive is now a custom install in the user folder and needs corresponding environment variables
    - add VS Code executable to path

--- configs/path.zsh
+++ configs/path.zsh
@@ -1,18 +1,26 @@
-# for homebrew
-export PATH="/usr/local/sbin:$PATH"
+# Homebrew (has its own logic for updating $PATH, so just run that)
+eval "$(/opt/homebrew/bin/brew shellenv)"
 
-# for python environment
-export PYTHONPATH="$PYTHONPATH:/usr/local/opt/python/bin"
-export PATH="$PATH:/usr/local/opt/python/bin"
-
-# for flutter environment
-export PATH="$PATH:$HOME/Library/flutter/bin"
-
-# for android environment
-export ANDROID_HOME="$HOME/Library/Android"
-export PATH="$PATH:$ANDROID_HOME/cmdline-tools/latest/bin"
-export PATH="$PATH:$ANDROID_HOME/emulator"
-export PATH="$PATH:$ANDROID_HOME/platform-tools"
-
-# for rust (has its own logic for updating $PATH, so just run that)
+# Rust (has its own logic for updating $PATH, so just run that)
 source "$HOME/.cargo/env"
+
+# Python default virtual environment (via uv)
+export PATH="$HOME/.local/uv-default/.venv/bin:$PATH"
+
+# TeXLive manual local install (update each year)
+export PATH="$PATH:$HOME/.local/texlive/2025basic/bin/universal-darwin"
+export TEXMFHOME="$HOME/.local/texlive/2025basic/texmf"
+export TEXMFCONFIG="$HOME/.local/texlive/2025basic/texmf-config"
+export TEXMFVAR="$HOME/.local/texlive/2025basic/texmf-var"
+
+# Flutter
+# export PATH="$PATH:$HOME/Library/flutter/bin"
+
+# Android
+# export ANDROID_HOME="$HOME/Library/Android"
+# export PATH="$PATH:$ANDROID_HOME/cmdline-tools/latest/bin"
+# export PATH="$PATH:$ANDROID_HOME/emulator"
+# export PATH="$PATH:$ANDROID_HOME/platform-tools"
+
+# VS Code
+export PATH="/Applications/Visual Studio Code.app/Contents/Resources/app/bin:$PATH"
