beaa9848b84ba4c7add8a89894d70994f5af395b
Niklas Bogensperger <niklasbogensperger@users.noreply.github.com>
2025-04-22 22:28:24 +0200

    Update 09_uv-default-python-upgrade.zsh
    We can simply delete everything except the pyproject.toml file and re-lock/re-sync the environment, which will automatically use the new python version from the PATH.
    The previous solution with an export step to a requirements.txt file followed by a reinitialization caused the package versions in the new pyproject.toml to all be pinned (== instead of >=), making the environment unupgradable without manual editing of pyproject.toml.

--- functions/09_uv-default-python-upgrade.zsh
+++ functions/09_uv-default-python-upgrade.zsh
@@ -1,20 +1,13 @@
 function uv-default-python-upgrade() {
-  local timestamp=$(date +"%Y%m%d_%H%M%S")
-  local backup_file="$HOME/requirements_$timestamp.txt"
+  echo "\n\033[34m==>\033[0m Deleting current uv lock file and .venv in $HOME/.local/uv-default..."
+  rm -rf $HOME/.local/uv-default/uv.lock
+  rm -rf $HOME/.local/uv-default/.venv
 
-  echo "\033[34m==>\033[0m Exporting current uv environment requirements to $backup_file..."
-  uv-default export -o "$backup_file"
+  echo "\n\033[34m==>\033[0m Generating new uv lock file from leftover pyproject.toml..."
+  uv-default lock
 
-  echo "\n\033[34m==>\033[0m Deleting old uv-default environment at $HOME/.local/uv-default..."
-  rm -rf $HOME/.local/uv-default
+  echo "\n\033[34m==>\033[0m Re-adding packages to environment..."
+  uv-default sync
 
-  echo "\n\033[34m==>\033[0m Initializing new uv environment at $HOME/.local/uv-default..."
-  uv init $HOME/.local/uv-default
-
-  echo "\n\033[34m==>\033[0m Adding packages from backup file..."
-  uv-default add -r "$backup_file"
-
-  echo "\n\033[32m==>\033[0m Update of uv environment completed."
-  echo "\nBackup requirements file is located at: $backup_file"
-  echo "\033[1mTo remove the backup, run:\033[0m rm $backup_file"
+  echo "\n\033[32m==>\033[0m Upgrade of python version in uv-default environment completed."
 }
